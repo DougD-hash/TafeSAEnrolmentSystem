63d6a08b333c7163e465493d817319ceb4b3151b
DougD-hash <douglas.davies2@student.tafesa.edu.au>
2025-02-18 15:34:01 +1100

    Person class first commit
    Still needs to have equals methods overridden

--- TafeSAEnrolmentSystem/TafeSAEnrolmentSystem/Person.cs
+++ TafeSAEnrolmentSystem/TafeSAEnrolmentSystem/Person.cs
@@ -8,5 +8,36 @@ namespace TafeSAEnrolmentSystem
 {
     class Person
     {
+        const string DEF_NAME = "No name provided";
+        const string DEF_EMAIL = "No email provided";
+        const string DEF_PHONE = "No phone number provided";
+
+        //private string name;
+        //private string email;
+        //private string phoneNumber;
+
+        public string PersonName { get; set; }
+
+        public string PersonEmail { get; set; }
+
+        public string PersonPhoneNumber { get; set; }
+
+        public Person() : this (DEF_NAME, DEF_EMAIL, DEF_PHONE)
+        {
+
+        }
+
+        public Person(string name, string email, string phonenumber)
+        {
+            PersonName = name;
+            PersonEmail = email;
+            PersonPhoneNumber = phonenumber;
+        }
+
+        public override string ToString()
+        {
+            return "Name :" + PersonName + " Email: " + PersonEmail + " Phone Number: " + PersonPhoneNumber;
+        }
+
     }
 }
--- TafeSAEnrolmentSystem/TafeSAEnrolmentSystem/Program.cs
+++ TafeSAEnrolmentSystem/TafeSAEnrolmentSystem/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TafeSAEnrolmentSystem;
+
+Person myPerson = new Person();
+Console.WriteLine(myPerson);
+
+myPerson.PersonName = "John Doe";
+myPerson.PersonEmail = "Johndoe@outlook.com";
+myPerson.PersonPhoneNumber = "0425356472";
+Console.WriteLine("myPerson name: " + myPerson.PersonName);
+Console.WriteLine("myPerson email: " + myPerson.PersonEmail);
+Console.WriteLine("myPerson phone number: " + myPerson.PersonPhoneNumber);
+
+
+
+
+Console.ReadKey();
